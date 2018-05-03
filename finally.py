b118cc2bfbf5ea2aadef4843f1605aaea81fe443
florian <florian@meilleurescpi.com>
2018-05-03 16:04:24 +0200

    On va attaque la partie 2

---
+++ finally.py
@@ -0,0 +1,9 @@
+try:
+    # Test d'instruction(s)
+except type_de_l_exception:
+    # Traitement en cas d'erreur
+finally:
+    # Instruction(s) exécutée(s) qu'il y ait eu des erreurs ou non
+
+
+# finallypermet d'exécuter du code après un bloctry, quel que soit le résultat d# e l'exécution dudit bloc.
